57d095137526ba5fe2d8fc0ba40b9def59a34004
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 02:45:43 +0300

    Import the new schema and resolvers to the app

--- index.ts
+++ index.ts
@@ -1,21 +1,11 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const { typeDefs} = require('./schema')
+const { resolvers} = require('./resolvers')
 
 const PORT = 4000;
 
 const app = express();
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!'
-  },
-};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
