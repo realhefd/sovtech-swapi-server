0d5d51fa50af1d8e92c7118b5b057051f2cebdb5
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 03:57:39 +0300

    Refactore code, add comments

--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
 const { StarWarsAPI } = require('./datasources/people');
 const { ApolloServer } = require('apollo-server');
-const { typeDefs} = require('./schema')
-const { resolvers} = require('./resolvers')
+const { resolvers } = require('./resolvers');
+const { typeDefs } = require('./schema');
 
+// Pass schema definition, resolvers and data sources instance to the ApolloServer constructor
 const server: any = new ApolloServer({
     typeDefs,
     resolvers,
     dataSources: () => {
       return {
         people: new StarWarsAPI()
-      }
+      };
     }
   }
 );
 
 // Starting the grapgql server to serve the API endpoints.
-server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
+server.listen().then(({ url }: any) => console.log(`🚀  Server ready at ${url}`));
