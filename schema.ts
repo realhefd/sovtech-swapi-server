bfb9a89cca2a3e4f231d941b1e28a4396603c1df
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 04:16:04 +0300

    Refactore code

--- schema.ts
+++ schema.ts
@@ -1,20 +1,18 @@
 const { gql } = require('apollo-server');
+
 /** 
  * This GraphQL server uses this schema to describe the shape of the data graph as is from SWAPI API.
  * This schema defines a hierarchy of types with fields that are populated from the StarWarsAPI datastore.
  * The schema also specifies exactly which queries are available for clients to execute against the data graph.
  */
-
 export const typeDefs: string | string[] | any = gql`
-
-
+"Query type that describes the data that will be used for the people and details pages"
 type Query{
   getPeople(page: Int): People!
   getPerson(name: String): People!
 }
 
-
-
+"People type: consisting of pagination metadata and array of Persons objects"
 type People {
   count: Int!
   next: String
@@ -22,8 +20,7 @@ type People {
   results: [ Person! ]!
 }
 
-
-
+"HomeWorld type: Describes the home planet of the characters, only relevant fields picked"
 type HomeWorld{
   name: String!
   climate: String!
@@ -31,12 +28,12 @@ type HomeWorld{
   population: String!
 }
 
-
-
+"Person type: Describes a specific person within an array of persons"
 type Person{
   name: String!
   mass: String!
   height: String!
   gender: String!
   homeworld: HomeWorld!
-}`;
+}
+`;
