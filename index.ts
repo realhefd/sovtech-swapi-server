1e1bf2ec4f6516da87e77a706c553949c14f0bc9
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 03:02:39 +0300

    Purge express

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
-const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs} = require('./schema')
 const { resolvers} = require('./resolvers')
 
-const app = express();
-
 const server: any = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app });
-
 
 // Starting the grapgql server to serve the API endpoints.
 server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
