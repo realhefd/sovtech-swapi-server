b5e56e0ec9960d87e61e198053c5d47f317aa88d
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 03:31:29 +0300

    Plug in the StarWarsAPI datasources to the apollo server

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
+const { StarWarsAPI } = require('./datasources/people');
 const { ApolloServer } = require('apollo-server');
 const { typeDefs} = require('./schema')
 const { resolvers} = require('./resolvers')
 
-const server: any = new ApolloServer({ typeDefs, resolvers });
+const server: any = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: new StarWarsAPI()
+  }
+);
 
 // Starting the grapgql server to serve the API endpoints.
 server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
